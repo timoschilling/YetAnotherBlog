135925530de9651cf2db44c7db1979e4d61494b8
Timo Schilling <timo@schilling.io>
2016-02-16 23:54:47 +0100

    comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216225336) do
+ActiveRecord::Schema.define(version: 20160216225433) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "post_id",    limit: 4
+    t.string   "content",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -30,5 +41,7 @@ ActiveRecord::Schema.define(version: 20160216225336) do
     t.datetime "updated_at",             null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
