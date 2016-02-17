c4ba9d1c87e99b84fb22b8cfc14834ae70924509
Timo Schilling <timo@schilling.io>
2016-02-17 03:04:31 +0100

    real db time measurement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.includes(:user, comments: [:user]).limit(100)
+    ms = Benchmark.ms do
+      posts = Post.all.includes(:user, comments: [:user]).limit(100)
+      @post_count = posts.count
+      @posts = posts.to_a
+    end
+    puts "DB Queries: %0.2fms" % ms
   end
 end
